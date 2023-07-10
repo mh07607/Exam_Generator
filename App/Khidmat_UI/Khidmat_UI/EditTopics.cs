0bd6a379f9dd3b0f5dbd7b7eeabaf31f627e27fd
mh07607 <mh07607@st.habib.edu.pk>
2023-07-10 20:11:04 +0500

    new queries

--- App/Khidmat_UI/Khidmat_UI/EditTopics.cs
+++ App/Khidmat_UI/Khidmat_UI/EditTopics.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Khidmat_UI
 {
     public partial class EditTopics : Form
     {
+        const string connectionString = @"Data Source=DESKTOP-PEGIUMG\YEET; Initial Catalog = khidmat_test; Integrated Security = True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        SqlCommand command = new SqlCommand();
         public EditTopics()
         {
             InitializeComponent();
@@ -19,7 +24,14 @@ namespace Khidmat_UI
 
         private void EditTopics_Load(object sender, EventArgs e)
         {
-
+            connection.Open();
+            string sql = "SELECT * FROM TOPIC";
+            command = new SqlCommand(sql, connection);
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            command.Dispose();
+            connection.Close();
         }
     }
 }
