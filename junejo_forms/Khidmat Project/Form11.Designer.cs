9e3aca02b4312e28643f6ec199b450f087768ebe
Muzz309 <ms07164@st.habib.edu.pk>
2023-08-13 02:16:38 +0500

    Some work on Form11

--- junejo_forms/Khidmat Project/Form11.Designer.cs
+++ junejo_forms/Khidmat Project/Form11.Designer.cs
@@ -54,9 +54,11 @@
             this.groupBox1.Controls.Add(this.button6);
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(484, 73);
+            this.groupBox1.Location = new System.Drawing.Point(363, 59);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(318, 144);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(238, 117);
             this.groupBox1.TabIndex = 84;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Filters";
@@ -64,18 +66,20 @@
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(75, 61);
+            this.comboBox3.Location = new System.Drawing.Point(56, 50);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(216, 24);
+            this.comboBox3.Size = new System.Drawing.Size(163, 21);
             this.comboBox3.TabIndex = 54;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Segoe UI Variable Display", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(25, 63);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(19, 51);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(42, 17);
+            this.label5.Size = new System.Drawing.Size(40, 13);
             this.label5.TabIndex = 53;
             this.label5.Text = "Book:";
             // 
@@ -83,29 +87,34 @@
             // 
             this.button6.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button6.Font = new System.Drawing.Font("Segoe UI Variable Display", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(113, 95);
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(85, 77);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(127, 35);
+            this.button6.Size = new System.Drawing.Size(95, 28);
             this.button6.TabIndex = 20;
             this.button6.Text = "Filter";
             this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(75, 26);
+            this.comboBox1.Location = new System.Drawing.Point(56, 21);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(216, 24);
+            this.comboBox1.Size = new System.Drawing.Size(163, 21);
             this.comboBox1.TabIndex = 14;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Variable Display", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(11, 28);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 12;
             this.label1.Text = "Subject:";
             // 
@@ -113,10 +122,11 @@
             // 
             this.button4.BackColor = System.Drawing.Color.RosyBrown;
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button4.Font = new System.Drawing.Font("Segoe UI Variable Display", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(337, 457);
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(253, 371);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(138, 35);
+            this.button4.Size = new System.Drawing.Size(104, 28);
             this.button4.TabIndex = 83;
             this.button4.Text = "Back";
             this.button4.UseVisualStyleBackColor = false;
@@ -126,10 +136,11 @@
             // 
             this.button5.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button5.Font = new System.Drawing.Font("Segoe UI Variable Display", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(329, 168);
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(247, 136);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(130, 35);
+            this.button5.Size = new System.Drawing.Size(98, 28);
             this.button5.TabIndex = 82;
             this.button5.Text = "Add";
             this.button5.UseVisualStyleBackColor = false;
@@ -139,10 +150,11 @@
             // 
             this.button1.BackColor = System.Drawing.Color.RosyBrown;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button1.Font = new System.Drawing.Font("Segoe UI Variable Display", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(59, 168);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(44, 136);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 35);
+            this.button1.Size = new System.Drawing.Size(96, 28);
             this.button1.TabIndex = 81;
             this.button1.Text = "Delete";
             this.button1.UseVisualStyleBackColor = false;
@@ -151,10 +163,11 @@
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button2.Font = new System.Drawing.Font("Segoe UI Variable Display", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(193, 168);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(145, 136);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(130, 35);
+            this.button2.Size = new System.Drawing.Size(98, 28);
             this.button2.TabIndex = 80;
             this.button2.Text = "Edit";
             this.button2.UseVisualStyleBackColor = false;
@@ -164,56 +177,62 @@
             // 
             this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button3.Font = new System.Drawing.Font("Segoe UI Variable Display", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(329, 127);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(247, 103);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(130, 35);
+            this.button3.Size = new System.Drawing.Size(98, 28);
             this.button3.TabIndex = 79;
             this.button3.Text = "Search";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(120, 89);
+            this.textBox1.Location = new System.Drawing.Point(90, 72);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(339, 22);
+            this.textBox1.Size = new System.Drawing.Size(255, 20);
             this.textBox1.TabIndex = 78;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(59, 232);
+            this.dataGridView1.Location = new System.Drawing.Point(44, 188);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(686, 213);
+            this.dataGridView1.Size = new System.Drawing.Size(514, 173);
             this.dataGridView1.TabIndex = 77;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI Variable Display", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(56, 94);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 76);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.Size = new System.Drawing.Size(43, 13);
             this.label2.TabIndex = 76;
             this.label2.Text = "Topic:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI Variable Display", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(310, 20);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(232, 16);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(199, 33);
+            this.label3.Size = new System.Drawing.Size(165, 25);
             this.label3.TabIndex = 75;
             this.label3.Text = "Topics  Database";
             // 
             // Form11
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(849, 510);
+            this.ClientSize = new System.Drawing.Size(637, 414);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button5);
@@ -224,6 +243,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form11";
             this.Text = "Topics Database";
             this.groupBox1.ResumeLayout(false);
--- junejo_forms/Khidmat Project/Form11.cs
+++ junejo_forms/Khidmat Project/Form11.cs
@@ -2,19 +2,58 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Khidmat_Project
 {
+
     public partial class Form11 : Form
     {
+        const string connectionString = @"Data Source =DESKTOP-PEGIUMG; Inital Catalog = khidmat_test1; Integrated Security = False; user id =Admin;password=Blaze30083";
+        SqlConnection connection = new SqlConnection(connectionString);
+        SqlCommand command = new SqlCommand();
+        Dictionary<string, int> subjectName_Id = new Dictionary<string, int>();
+        Dictionary<string, int> BookName_Id = new Dictionary<string, int>();
+
         public Form11()
         {
             InitializeComponent();
+        }
+
+        private void Form11_Load(object sender, EventArgs e)
+        {
+            List<string> subjectList = getSubjects();
+            comboBox1.DataSource = subjectList;
+        }
+
+        private List<string> getSubjects()
+        {
+            List<string> subjectList = new List<string>();
+
+            connection.Open();
+            string query = "select * from Subject";
+            command = new SqlCommand(query, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int subjectId = Convert.ToInt32(reader["SubjectId"]);
+                string subjectName = reader["SubjectName"].ToString();
+
+                subjectName_Id[subjectName] = subjectId;
+
+                subjectList.Add(subjectName);
+            }
+
+            reader.Close();
+            command.Dispose();
+            connection.Close();
+            return subjectList;
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -36,6 +75,77 @@ namespace Khidmat_Project
             Form13 form13 = new Form13();
             form13.Show();
             this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Search Button
+        {
+            string topicName = textBox1.Text.ToString();
+            string query;
+            if(topicName == null)
+            {
+                query = "SELECT TopicName FROM Topic"; //Showing all topics
+            }
+            else
+            {
+                query = "SELECT TopicName FROM Topic WHERE TopicName LIKE %" + @topicName + "%"; //Showing topics based on entered data
+            }
+            connection.Open();
+            command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@topicName", topicName);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            command.Dispose(); 
+            connection.Close();
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void button6_Click(object sender, EventArgs e) //Filter Button
+        {
+            string topicName = textBox1.Text.ToString();
+            string subjectName = comboBox1.Text.ToString();
+            string bookName = comboBox3.Text.ToString();
+
+
+            string query = "SELECT TopicName FROM Topic WHERE 1=1 ";
+            
+            if(topicName != null)
+            {
+                query = query + " AND TopicName LIKE %" + "@topicName" + "%";
+                command.Parameters.AddWithValue("@topicName", topicName);
+            }
+            if(subjectName != null)
+            {
+                query = query + " AND SubjectId = @SubjectID ";
+                command.Parameters.AddWithValue("@SubjectID", subjectName_Id[subjectName]);
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) //Filling the book comboBox
+        {
+            //Updating the entries in comboBox2
+            List<string> BookList = new List<string>();
+
+            string subjectSelcted = comboBox1.Text.ToString();
+            string id = subjectName_Id[subjectSelcted].ToString();
+            connection.Open();
+            string query = "select BookName, BookID from Book where SubjectID = " + id;
+            command = new SqlCommand(query, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int BookId = Convert.ToInt32(reader["BookId"]);
+                string BookName = reader["BookName"].ToString();
+
+                subjectName_Id[BookName] = BookId;
+
+                BookList.Add(BookName);
+            }
+            reader.Close();
+            command.Dispose();
+            connection.Close();
+            comboBox3.DataSource = BookList;
         }
     }
 }
