b9fc7f3e6351639973a9b84c85a3863487e64e1c
Muzz309 <ms07164@st.habib.edu.pk>
2023-08-27 14:44:46 +0500

    Delete topics Updated
    Added functionality to also delete entries from the Past Paper Bridge Table for MCQ and Questions

--- junejo_forms/Khidmat Project/Form11.cs
+++ junejo_forms/Khidmat Project/Form11.cs
@@ -204,9 +204,25 @@ namespace Khidmat_Project
                 int selectedRowIndex = selectedCell.RowIndex;
                 int topicId = Convert.ToInt32(dataGridView1.Rows[selectedRowIndex].Cells[0].Value);
 
+                //Deleting from Past Paper Questions
+                connection.Open();
+                string query = "DELETE FROM Paper_Question WHERE QuestionID IN (SELECT QuestionID FROM Questions WHERE TopicID = @TopicID)";
+                command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@TopicId", topicId);
+                command.ExecuteNonQuery();
+                connection.Close();
+
+                //Deleting from Past Paper MCQs
+                connection.Open();
+                query = "DELETE FROM Paper_MCQ WHERE MCQID IN (SELECT MCQID FROM MCQs WHERE TopicID = @TopicID)";
+                command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@TopicId", topicId);
+                command.ExecuteNonQuery();
+                connection.Close();
+
                 //Deleting all assosicated Questions with the Topic
                 connection.Open();
-                string query = "DELETE FROM Questions WHERE TopicId = @TopicId";
+                query = "DELETE FROM Questions WHERE TopicId = @TopicId";
                 command = new SqlCommand(query, connection);
                 command.Parameters.AddWithValue("@TopicId", topicId);
                 command.ExecuteNonQuery();
@@ -235,6 +251,9 @@ namespace Khidmat_Project
                 command.Parameters.AddWithValue("@TopicId", topicId);
                 command.ExecuteNonQuery();
                 connection.Close();
+
+                this.button6_Click(sender, e);
+                MessageBox.Show("Topic and all assosicated data deleted sucessfully");
             }
         }
     }
